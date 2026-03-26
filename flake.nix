bfbc1424bdc7192653e1b03d009d4f1738f242a7
Amadeus Mader <amadeus@mozart409.com>
2026-03-27 00:02:16 +0100

    chore: use rust 1.94

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
         config.allowUnfree = true;
         overlays = [rust-overlay.overlays.default];
       };
-      rust = pkgs.rust-bin.stable."1.92.0".default;
+      rust = pkgs.rust-bin.stable."1.94.0".default;
     in {
       # to use other shells, run:
       # nix develop . --command fish
